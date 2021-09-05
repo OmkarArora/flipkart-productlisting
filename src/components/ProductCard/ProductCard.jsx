671223bbb3aa627b962513eb996e80234435d7e4
Omkar Arora <aroraomkar@gmail.com>
2021-09-05 12:57:44 +0530

    style: products layout

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,12 +7,14 @@ export const ProductCard = ({ _id, name, brand, price, discount, sizes, ideal, i
 			<div className="container-product-img">
 				<img src={images[0]} alt={name} />
 			</div>
-			<div className="brand-name text-grey weight500">{brand}</div>
-			<div className="product-name">{name}</div>
-			<DiscountedPrice currency={currency} price={price} discount={discount} />
-			<div>
-				<span className="text-grey weight500">Size </span>
-				{sizes.join(",")}
+			<div className="product-details">
+				<div className="brand-name text-grey weight500">{brand}</div>
+				<div className="product-name">{name}</div>
+				<DiscountedPrice currency={currency} price={price} discount={discount} />
+				<div>
+					<span className="text-grey weight500">Size </span>
+					{sizes.join(",")}
+				</div>
 			</div>
 		</div>
 	)
